7e377315e6c49708d8d70ffd34226b31071a5870
chaiwat47 <58399208+chaiwat47@users.noreply.github.com>
2020-01-24 12:18:55 +0700

    Update gradient_descent_algorithm.py

--- gradient_descent_algorithm.py
+++ gradient_descent_algorithm.py
@@ -1,6 +1,7 @@
 # find minimum point1 for  y=x^2
 print("GD function for y=x^2")
-def find_min_point1(learning_rate,epsilon,x_before,dif):
+def find_min_point1(learning_rate):
+    epsilon,x_before,dif=0.0000001,5,1
     while(dif>epsilon):
         x_updated=x_before-learning_rate*(2*x_before)
         dif=abs(x_before-x_updated)
@@ -8,18 +9,19 @@ def find_min_point1(learning_rate,epsilon,x_before,dif):
     return x_updated
 #testf1
 print("test for minimum point of y=x^2")
-test1=find_min_point1(learning_rate=0.1, epsilon=0.0000001, x_before=5, dif=1)
+test1=find_min_point1(learning_rate=0.1)
 print("test_1=",test1)
 
-test2=find_min_point1(learning_rate=0.01, epsilon=0.0000001, x_before=5, dif=1)
+test2=find_min_point1(learning_rate=0.01)
 print("test_2=",test2)
 
-test3=find_min_point1(learning_rate=0.5, epsilon=0.0000001, x_before=5, dif=1)
+test3=find_min_point1(learning_rate=0.5)
 print("test_3=",test3)
 
 # find minimum point2 for  y=(x+1)^2
 print("GD function for y=(x+1)^2")
-def find_min_point2(learning_rate,epsilon,x_before,dif):
+def find_min_point2(learning_rate):
+    epsilon,x_before,dif=0.0000001,5,1
     while(dif>epsilon):
         x_updated=x_before-learning_rate*(2*(x_before+1))
         dif=abs(x_before-x_updated)
@@ -28,11 +30,11 @@ def find_min_point2(learning_rate,epsilon,x_before,dif):
 
 #testf2
 print("test for minimum point of y=(x+1)^2")
-test1=find_min_point2(learning_rate=0.1, epsilon=0.0000001, x_before=5, dif=1)
+test1=find_min_point2(learning_rate=0.1)
 print("test_x1=",test1)
 
-test2=find_min_point2(learning_rate=0.01, epsilon=0.0000001, x_before=5, dif=1)
+test2=find_min_point2(learning_rate=0.01)
 print("test_x2=",test2)
 
-test3=find_min_point2(learning_rate=0.5, epsilon=0.0000001, x_before=5, dif=1)
+test3=find_min_point2(learning_rate=0.5)
 print("test_x3=",test3)
